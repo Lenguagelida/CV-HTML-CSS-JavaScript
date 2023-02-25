fcb819b16bcecf6d3512d243b01b21d8c9273403
DanielDab <dab777799@gmail.com>
2023-02-24 22:08:08 -0300

    Api conectada

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-        console.log(data);
+const btnCambiarPersona = document.getElementById('btnCambiar');
+const url = 'https://randomuser.me/api/';
+const foto = document.getElementById('fotoUsuario');
+const nombre = document.getElementById('nombre');
+const email = document.getElementById('email');
+
+const generarPersona = async() => {
+    try {
+        let response = await fetch(url);
+        const data = await response.json();
+        let objetoPersona = data.results[0];
+        console.log(objetoPersona);
+
+        foto.src = objetoPersona.picture.large;
+        nombre.textContent = objetoPersona.name.first + " " +  objetoPersona.name.last;
+        email.textContent = "Email: "+ objetoPersona.email;
+
+    } catch (error) {
+        console.log(error);
     }
-});
+}
+
+
+
+btnCambiarPersona.addEventListener('click',generarPersona);
